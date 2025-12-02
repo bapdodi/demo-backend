0b08710354d0289a9e406fb30186dd7b3e0ed47f
Kim Jeong Bin <101189767+bapdodi@users.noreply.github.com>
2025-12-02 21:40:56 +0900

    Update WebConfig.java

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
                 .allowedOrigins("http://161.97.130.200:30610") // [중요] 프론트엔드 접속 주소 허용
-                // .allowedOriginPatterns("*") // 테스트용으로 모든 주소 다 열고 싶다면 이걸 사용
+                .allowedOriginPatterns("*") // 테스트용으로 모든 주소 다 열고 싶다면 이걸 사용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*")
                 .allowCredentials(true);
